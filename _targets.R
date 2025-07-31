473ceda532c32d7ced1cb5c43ea24a926954ebbb
c1au6i0 <YourNewEmail@example.com>
2025-07-31 00:32:48 -0400

    feat: subtraction orange

--- _targets.R
+++ _targets.R
@@ -151,7 +151,9 @@ list(
       dplyr::mutate(CID = as.integer(CID)) |> 
       dplyr::right_join(ex_cid_chem, by ="CID") |> 
       dplyr::relocate(CASRN:CID, .after = IUPACName) |> 
-      dplyr::distinct(CID, .keep_all = TRUE)
+      dplyr::distinct(CID, .keep_all = TRUE) |> 
+      dplyr::select(IUPACName:InChIKey) |> 
+      dplyr::select(-MolecularWeight)
   ),
   tar_file(
     ex_cid_chem_out,
@@ -160,7 +162,6 @@ list(
       ex_cid_chem_all |> 
         write.xlsx(path_out)
       path_out
-      
     }
   ),
   # @@@@@@@@@@@@@@@@@@
@@ -169,17 +170,79 @@ list(
   tar_target(
     solution,
     ex_cid_chem_all |> 
-      dplyr::filter(!SMILES %in% main_chem_all$SMILES)
+      dplyr::filter(!SMILES %in% main_chem_all$SMILES) |> 
+      dplyr::select(IUPACName:InChIKey)
   ),
   tar_file(
     solution_out,
     {
-      path_out <- here::here("reports", "tabs_figs", "inner_to_check.xlsx")
+      path_out <- here::here("reports", "tabs_figs", "outer_to_check.xlsx")
       solution |> 
         write.xlsx(path_out)
       path_out
       
     }
+  ),
+  # @@@@@@@@@@@@@@@@@@
+  # orange ----@@@@@@@
+  # @@@@@@@@@@@@@@@@@@
+  tar_file_read(
+    orange,
+    here::here("data", "other", "orange.xlsx"),
+    readxl::read_xlsx(path = !!.x) |> 
+      janitor::clean_names() |> 
+      dplyr::select(1:3) |> 
+      mutate(casrn = str_trim(casrn))
+  ),
+  tar_target(
+    orange_cid,
+    orange |> 
+      filter(!is.na(casrn)) |> 
+      mutate(casrn = str_trim(casrn)) |> 
+      dplyr::distinct(casrn) |> 
+      pull(casrn) |> 
+      webchem::get_cid(match = "first",
+                       verbose = TRUE)
+  ),
+  tar_target(
+    to_add_orange,
+    tibble::tribble(
+      ~query, ~cid,
+      "27043-05-6", "26334"
+    )
+  ),
+  tar_target(
+    orange_cid_all,
+    orange_cid |> 
+      dplyr::filter(!is.na(cid)) |> 
+      dplyr::bind_rows(to_add_orange) 
+  ),
+  tar_target(
+    orange_chem,
+    orange_cid_all |>
+      dplyr::bind_rows(to_add_orange) |>
+      dplyr::distinct(cid) |>
+      tidyr::drop_na(cid) |>
+      pull(cid) |>
+      webchem::pc_prop(verbose = TRUE) |>
+      dplyr::relocate(IUPACName, 1) 
+  ),
+  tar_target(
+    orange_chem_all,
+    orange_cid_all |> 
+      dplyr::rename(CASRN = query, CID = cid) |> 
+      dplyr::mutate(CID = as.numeric(CID)) |> 
+      dplyr::right_join(orange_chem, by ="CID") |> 
+      dplyr::relocate(CASRN:CID, .after = IUPACName) |>
+      dplyr::distinct(CID, .keep_all = TRUE) |>
+      dplyr::select(IUPACName:InChIKey)
+  ),
+  tar_target(
+    orange_plus,
+    orange_chem_all |> 
+      bind_rows(solution)  |> 
+      distinct(CID, .keep_all = TRUE)  
+    
   )
-  
+
 )
